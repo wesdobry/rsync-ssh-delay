f67b6919070b8f70ddfce34a79de4d9d0dce15ca
wesdobry <wesdobry@wesdobry.com>
2022-07-06 14:37:03 -0400

    fixed newlines

--- root/opt/rsync-ssh-delay/entrypoint.sh
+++ root/opt/rsync-ssh-delay/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "rsync container with wrapper delay script\n\n"
+echo -e "rsync container with wrapper delay script\n\n"
 
 while :
 do
-    echo "rsyncing $SOURCE to $DESTINATION"
-    echo "Command: rsync $ARGS $SOURCE $DESTINATION\n\n"
+    echo -e "rsyncing $SOURCE to $DESTINATION"
+    echo -e "Command: rsync $ARGS $SOURCE $DESTINATION\n\n"
 
     rsync $ARGS $SOURCE $DESTINATION
 
-    echo "\n\nSleeping for $DELAY seconds\n\n"
+    echo -e "\n\nSleeping for $DELAY seconds\n\n"
     sleep $DELAY
 done
